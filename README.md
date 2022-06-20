5b6eac92b1739df783a65a4f7918046a9c4be781
danilasykulev <dsykulev@digital-spirit.ru>
2022-06-20 13:12:39 +0300

    + README.md & little refactor

--- README.md
+++ README.md
@@ -2,3 +2,63 @@
 ![GoCoverage](https://img.shields.io/badge/Coverage-80.7%25-green)
 # go-conc
 Concurrency patterns realization in Golang
+
+# Worker Pool
+
+Collect Tasks and executes them into routines concurrently. 
+Has a watchdog routine that looks after worker routines and tasks queue (channel) length. 
+If task queue accumulating tasks then watchdog creates new workers but no more than pool size.
+On the over side if where is idle workers which size is more than allowed in pool settings 
+than watchdog kills exceed idlers. 
+
+```go
+type Options struct {
+	Size          uint32 // Pool size, max workers count
+	TaskChSize    uint32 // Max task queue (channel) length
+	ResultChSize  uint32 // Max result channel length
+	Idle          uint32 // Max idle workers count
+	RecoveryFn    func() // Custom recovery func to use in workers
+	WatcherPeriod time.Duration // Watchdog action timeout
+}
+```
+## Usage
+```go
+...
+        ctx := context.TODO()
+	ctx, cancel := context.WithCancel(ctx)
+	opts := wp.GetDefaultOptions()
+	opts.RecoveryFn = func() {
+		if msg := recover(); msg != nil {
+			fmt.Println(msg)
+			cancel()
+		}
+	}
+	pool := wp.NewWorkerPool(opts)
+	pool.Run(ctx)
+	for i := 0; i < 10; i++ {
+		pool.Add(wp.NewTask(
+			func(ctx context.Context, args ...interface{}) (interface{}, error) {
+				return args[0].(int), nil
+			}, i))
+	}
+
+	pool.Add(wp.NewTask(func(ctx context.Context, args ...interface{}) (interface{}, error) {
+		return nil, fmt.Errorf("error")
+	}))
+	pool.Add(wp.NewTask(func(ctx context.Context, args ...interface{}) (interface{}, error) {
+		panic("oops! panic!")
+	}))
+	for {
+		select {
+		case <-pool.Done():
+			return
+		case result := <-pool.Result():
+			if result.GetErr() != nil {
+				fmt.Println(result.GetErr())
+				continue
+			}
+			fmt.Println(result.GetValue())
+		}
+	}
+...
+```
--- worker-pool-example_test.go
+++ worker-pool-example_test.go
@@ -2,6 +2,7 @@ package conc
 
 import (
 	"context"
+	"math/rand"
 	"runtime"
 	"testing"
 
@@ -48,4 +49,90 @@ func TestWorkerPoolExample(t *testing.T) {
 	assert.Equal(t, uint32(0), stats.WorkersCount)
 	assert.Equal(t, uint32(0), stats.Idlers)
 	assert.Equal(t, uint32(0), stats.TasksInQueue)
+}
+
+func BenchmarkWorkerPoolMergeSort(b *testing.B) {
+	in := genIntSlice()
+	for i := 0; i < b.N; i++ {
+		ctx := context.Background()
+		ctx, cancel := context.WithCancel(ctx)
+		parts := 2
+		chunkLen := len(in) / parts
+		tail := len(in) % parts
+		options := wp.GetDefaultOptions()
+		if runtime.GOMAXPROCS(0) == 1 {
+			options.Size = 2
+		}
+		options.Idle = uint32(parts)
+		pool := wp.NewWorkerPool(options)
+
+		for j := 0; j < parts; j++ {
+			startIdx := j * chunkLen
+			endIdx := (j + 1) * chunkLen
+			if j == parts-1 {
+				endIdx += tail
+			}
+			pool.Add(wp.NewTask(
+				func(ctx context.Context, args ...interface{}) (interface{}, error) {
+					return mSort(args[0].([]int)), nil
+				}, in[startIdx:endIdx]))
+		}
+		pool.Run(ctx)
+		var got []int
+		for j := 0; j < parts; j++ {
+			result := <-pool.Result()
+			got = mrg(got, result.GetValue().([]int))
+		}
+		cancel()
+		<-pool.Done()
+	}
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	in := genIntSlice()
+	for i := 0; i < b.N; i++ {
+		mSort(in)
+	}
+}
+
+func genIntSlice() (out []int) {
+	size := 1000
+	out = make([]int, size)
+	rand.Seed(1111)
+	for i := 0; i < size; i++ {
+		out[i] = rand.Intn(1000000)
+	}
+	return out
+}
+
+func mSort(slice []int) []int {
+
+	if len(slice) < 2 {
+		return slice
+	}
+	mid := (len(slice)) / 2
+	return mrg(mSort(slice[:mid]), mSort(slice[mid:]))
+}
+
+func mrg(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
 }
